dab6fa04bba8ecb0c4f86ef1f72b2b8d3b6f9be6
antoHolz <74118267+antoHolz@users.noreply.github.com>
2023-01-19 19:51:05 +0100

    Add files via upload

--- visualize_8.py
+++ visualize_8.py
@@ -17,11 +17,13 @@ def vis(argv):
     #     plt.plot(data_x_PID.iloc[:,1][NUM_WP*i:NUM_WP*(1+i)], data_y_PID.iloc[:,1][NUM_WP*i:NUM_WP*(1+i)], label=('PID_round'+str(i)))
 
     #### BO #########################################
+    
     csv_name=argv[1]#'pid_BO_R_500_alpha_UCB_c1_0.8_c2_4_start_PID_0.01_rounds_11-11.28.2022_21.29'
     data_x=pd.read_csv('../files/csvs/'+csv_name+'/x0.csv')
     data_y=pd.read_csv('../files/csvs/'+csv_name+'/y0.csv')
-    for i in range(int((data_x.shape[0])/NUM_CSTEPS)):
-        if(i==(int(data_x.shape[0]/NUM_CSTEPS)-1)):
+
+    for i in range(int((data_x.shape[0]+NUM_R)/NUM_CSTEPS)):
+        if(i==(int((data_x.shape[0]+NUM_R)/NUM_CSTEPS)-1)):
             plt.plot(data_x.iloc[:,1][NUM_CSTEPS*i:NUM_CSTEPS*i+NUM_WP], 
                     data_y.iloc[:,1][NUM_CSTEPS*i:NUM_CSTEPS*i+NUM_WP], 
                     label=('round'+str(i) + '(best)'), color='purple', linewidth=2)
@@ -38,19 +40,12 @@ def vis(argv):
     y_true=R * np.sin(2*((i / NUM_WP) * (2 * np.pi) + np.pi / 2))/2
     plt.plot(x_true,y_true,label='true_value',linestyle='dashed')
 
-    #### average cost per parameter combination ######
-    data_performance=pd.read_csv('../files/csvs/'+csv_name+'/candidate_performance.csv')
-    #mean_performance=data_performance.mean()
-    #print(mean_performance)
-    for col in data_performance.columns:
-        print(col, data_performance[col][0], data_performance[col][1],data_performance[col][2] )
-
-    #print("mean: "+str(np.std(data_performance.iloc[1,:])))
-    #print("std: " +str(np.std(data_performance.iloc[1,:])))
-
     #plt.legend()
     plt.savefig('../files/csvs/'+csv_name+'/trajectories.png')
     plt.show()
+
+    #### average cost per parameter combination ######
+    data_performance=pd.read_csv('../files/csvs/'+csv_name+'/candidate_performance.csv')
 
     iterations=np.arange(0,data_performance.columns.size-1,1)
     performance=[-float(i) for i in data_performance.iloc[1,1:]]
@@ -58,10 +53,17 @@ def vis(argv):
     plt.savefig('../files/csvs/'+csv_name+'/cost.png')
     plt.show()
 
-    # iterations=np.arange(0,data_performance.columns.size-2,1)
-    # performance=[-float(i) for i in data_performance.iloc[2,2:]]
-    # plt.plot(iterations, performance)
-    # plt.show()
+    for col in data_performance.columns:
+        print(col, data_performance[col][0], data_performance[col][1],data_performance[col][2] )
+
+
+    with open('../files/csvs/'+csv_name+'/performance.txt', 'w') as f:
+        for col in data_performance.columns:
+            f.write('\n'+str(col)+')'+ ' theta: '+str(data_performance[col][0])
+            +'  normalized cost: '+str(data_performance[col][1])
+            +'  absolute cost: '+str(data_performance[col][2]) )
+
+
 
 
 if __name__ == "__main__":
