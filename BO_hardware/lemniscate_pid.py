c11adc4dea3947259b8e9568b80a09ebb3a0e5df
antoHolz <74118267+antoHolz@users.noreply.github.com>
2023-02-28 17:18:43 +0000

    Add files via upload

--- BO_hardware/lemniscate_pid.py
+++ BO_hardware/lemniscate_pid.py
@@ -2,12 +2,13 @@
 
 import csv
 import numpy as np
+import warnings
 from sklearn.neighbors import NearestNeighbors
 from pycrazyswarm import *
 
-import sys
-sys.path.insert(0, "/home")
-from SafeOpt import safeopt
+#import sys
+#sys.path.insert(0, "/home/antonia")
+#from SafeOpt import safeopt
 
 Z = 1.
 
@@ -52,30 +53,50 @@ if __name__ == "__main__":
         wr.writerow(["P_x", "P_y", "P_z", "I_x", "I_y", "I_z", "cost", "norm_cost"])
 
     #init soll trajectory
-    X_soll=np.zeros(360)
+    X_soll=np.zeros((360,3))
     for i, t in enumerate(range(90, 360 + 90)):
         new_angle = deg2rad(t)
         X_soll[i] = get_postion(new_angle)
 
-    for i in range(360):
-        for cf in allcfs.crazyflies:
-            cf.cmdPosition(X_soll[i], 0,)
-            position=cf.position()
-            with open(csv_file_name, 'a', newline='') as myfile:
-                wr = csv.writer(myfile)
-                wr.writerow(position)
-            
-            if(i==0):
-                X_ist=np.expand_dims(position,0)
-            else:
-                X_ist=np.concatenate((X_ist, np.expand_dims(position,0)))
-        
-        timeHelper.sleep(0.05)
 
-    #calculate performance
-    nbrs=NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(X_ist)
-    distances, indices = nbrs.kneighbors(X_soll)
-    cost=-np.sum(np.sqrt(np.sqrt(distances)))
+    for rounds in range(2):
+        for i in range(360):
+           for cf in allcfs.crazyflies:
+                cf.cmdPosition(X_soll[i], 0,)           
+                if(i==0):
+                    X_ist=np.expand_dims(cf.position(),0)
+                else:
+                    X_ist=np.concatenate((X_ist, np.expand_dims(cf.position(),0)))
+           timeHelper.sleep(0.05)
+        
+        # Calculate performance
+        nbrs=NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(X_ist)
+        distances, indices = nbrs.kneighbors(X_soll)
+        cost=-np.sum(np.sqrt(np.sqrt(distances)))
+        if rounds==0:
+            norm=int(-cost)+1
+        n_cost=cost/norm
+  
+        # Get PID parameters	  
+        P_x=cf.getParam("posCtlPid/xKp")
+        P_y=cf.getParam("posCtlPid/yKp")
+        P_z=cf.getParam("posCtlPid/zKp")
+        I_x=cf.getParam("posCtlPid/xKi")
+        I_y=cf.getParam("posCtlPid/yKi")
+        I_z=cf.getParam("posCtlPid/zKi")
+    
+        # Save Trajectory
+        with open(csv_file_name, 'a', newline='') as myfile:
+            wr = csv.writer(myfile)
+            for row in list(X_soll):
+                 wr.writerow(row)
+
+        # Save Performance
+        performance=list((P_x,P_y,P_z, I_x,I_y,I_z, cost,n_cost))
+        with open(csv_performance, 'a', newline='') as myfile:
+            wr = csv.writer(myfile)
+            wr.writerow(performance)
+        print("cost: "+str(cost))
 
     
 
