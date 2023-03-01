b128c8c7cf87fc827994637a1ebaa5f790dafb51
antoHolz <74118267+antoHolz@users.noreply.github.com>
2023-03-01 10:25:30 +0000

    General order

--- BO_hardware/lemniscate_SO.py
+++ BO_hardware/lemniscate_SO.py
@@ -10,13 +10,10 @@ import sys
 sys.path.insert(0, "/home/antonia")
 from SafeOpt import safeopt
 
-Z = 1.
-
-
-def get_postion(phi):
+def get_postion(phi, z):
     x = 2*np.cos(phi)
     y = 2*np.sin(2*phi) / 2
-    return np.array([x, y, Z])
+    return np.array([x, y, z])
 
 
 def deg2rad(angle):
@@ -24,25 +21,20 @@ def deg2rad(angle):
 
 
 if __name__ == "__main__":
-    csv_file_name = "/home/antonia/trajectories.csv"  # tajectories_wind
-    csv_performance= "/home/antonia/performance.csv"
-
+    
+    #### Init swarm #####################################
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
     TIMESCALE = 1.0
 
-    allcfs.takeoff(targetHeight=Z, duration=2.0)
-    timeHelper.sleep(2.5)
+    #### Init learning ###################################
+    learn=True
+    learn_counter=0
 
-    # print parameters
-    # for cf in allcfs.crazyflies:
-    #     print(cf.getParam("posCtlPid/xKp"))
-    #     print(cf.getParam("posCtlPid/xKi"))
-    #     print(cf.getParam("posCtlPid/xKd"))
-    #     print(cf.getParam("posCtlPid/yKp"))
-    #     print(cf.getParam("posCtlPid/yKi"))
-    #     print(cf.getParam("posCtlPid/yKd"))
+    #### Init save files #################################
+    csv_file_name = "/home/antonia/trajectories.csv"  
+    csv_performance= "/home/antonia/performance.csv"
 
     with open(csv_file_name, 'w', newline='') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
@@ -52,12 +44,16 @@ if __name__ == "__main__":
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(["P_x", "P_y", "P_z", "I_x", "I_y", "I_z", "cost", "norm_cost"])
 
-    #init soll trajectory
+    #### Init soll trajectory ############################
+    Z_start=1.
     X_soll=np.zeros((360,3))
     for i, t in enumerate(range(90, 360 + 90)):
         new_angle = deg2rad(t)
-        X_soll[i] = get_postion(new_angle)
-
+        X_soll[i] = get_postion(new_angle,Z_start)
+ 
+    #### Flight start ####################################
+    allcfs.takeoff(targetHeight=Z_start, duration=2.0)
+    timeHelper.sleep(2.5)
 
     for rounds in range(2):
         for i in range(360):
@@ -69,7 +65,7 @@ if __name__ == "__main__":
                     X_ist=np.concatenate((X_ist, np.expand_dims(cf.position(),0)))
            timeHelper.sleep(0.05)
         
-        # Calculate performance
+        #### Calculate performance ###########################
         nbrs=NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(X_ist)
         distances, indices = nbrs.kneighbors(X_soll)
         cost=-np.sum(np.sqrt(np.sqrt(distances)))
@@ -77,7 +73,7 @@ if __name__ == "__main__":
             norm=int(-cost)+1
         n_cost=cost/norm
   
-        # Get PID parameters	  
+        #### Get PID parameters ##############################	  
         P_x=cf.getParam("posCtlPid/xKp")
         P_y=cf.getParam("posCtlPid/yKp")
         P_z=cf.getParam("posCtlPid/zKp")
@@ -85,13 +81,13 @@ if __name__ == "__main__":
         I_y=cf.getParam("posCtlPid/yKi")
         I_z=cf.getParam("posCtlPid/zKi")
     
-        # Save Trajectory
+        #### Save Trajectory #################################
         with open(csv_file_name, 'a', newline='') as myfile:
             wr = csv.writer(myfile)
             for row in list(X_soll):
                  wr.writerow(row)
 
-        # Save Performance
+        #### Save Performance #################################
         performance=list((P_x,P_y,P_z, I_x,I_y,I_z, cost,n_cost))
         with open(csv_performance, 'a', newline='') as myfile:
             wr = csv.writer(myfile)
@@ -99,6 +95,7 @@ if __name__ == "__main__":
         print("candidate: ("+ str(P_x)+str(P_y)+str(P_z)+str(I_x)+
         str(I_y)+str(I_z)+")    cost: "+str(cost))
         
+        #### Wait before starting new round ###################
         timeHelper.sleep(0.5)
 
     
